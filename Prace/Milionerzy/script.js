956316c2ffdffe15f0b39f2bdc8dc0e9fe5a3274
Sniadek <wojokarolg9@gmail.com>
2017-06-18 00:36:11 +0200

    Add files via upload

--- Prace/Milionerzy/script.js
+++ Prace/Milionerzy/script.js
@@ -91,23 +91,23 @@ document.addEventListener("DOMContentLoaded", function wywolanie() {
   changing_questions();
   buttons();
 });
-var number = 0;
-var win_score = 0;
-var getting_board = document.querySelector('.board');
-var getting_question = getting_board.querySelector('.question');
-var getting_answer = getting_board.querySelector('.answers');
-var getting_answers = getting_answer.querySelectorAll('.answer');
-var getting_prize = getting_board.querySelector('.prize');
-var getting_awards = getting_prize.querySelectorAll('.award');
-var this_is=undefined;
-function changing_questions() {
-  get_number();
-  if (questions[number].been_or_not === true) {
+var number = 0;     //przekazywanie wylosowanej liczby do wyboru zestawu pytań, global scope żeby kilka funkcji mogło z niej korzystać
+var win_score = 0;  // wynik, im większy tym więcej zaznaczonych pól z nagrodą
+var getting_board = document.querySelector('.board');                               //łapanie jakichś elementów
+var getting_question = getting_board.querySelector('.question');          //łapanie jakichś elementów
+var getting_answer = getting_board.querySelector('.answers');         //łapanie jakichś elementów
+var getting_answers = getting_answer.querySelectorAll('.answer');         //łapanie jakichś elementów
+var getting_prize = getting_board.querySelector('.prize');          //łapanie jakichś elementów
+var getting_awards = getting_prize.querySelectorAll('.award');          //łapanie jakichś elementów
+var this_is=undefined;        //przekazywanie z funkcji aktualnego przycisku do global scope żeby inna funkcja mogła to zobaczyć
+function changing_questions() {       // funkcja losująca pytania
+  get_number();     //wywoływanie funkcji, która losuję zestaw pytań
+  if (questions[number].been_or_not === true) {   //sprawdzanie czy dany zestaw się nie powtórzył, jeżeli tak zestaw pytań jest losowany od nowa
     changing_questions();
-  } else {
+  } else {      // jeżeli zestaw pytań nie wystąpił wrzucamy do divów zawartość pytań i odpowiedzi
     questions[number].been_or_not = true;
-    getting_question.textContent = questions[number].question;
-    for (i = 0; i < getting_answers.length; i++) {
+    getting_question.textContent = questions[number].question;    // wrzucanie pytania na tablice
+    for (i = 0; i < getting_answers.length; i++) {      //wrzucanie odpowiedzi do przycisków
       getting_answers[i].textContent = questions[number][`answer${ i }`];
       getting_answers[i].value = questions[number][`answer${ i }`];
     }
@@ -115,75 +115,78 @@ function changing_questions() {
 };
 
 function win() {
-  win_score++;
-  this_is.classList.remove('active_answer');
-  active_buttons();
-  if (win_score===10){
-    disable_buttons();
+  var awards=['100zł','500zł','5000zł','10000zł','25000zł','50000zł','100000zł','2500000zł','5000000zł','10000000zł'];
+  win_score++;  //inkrementujemy wynik
+  this_is.classList.remove('active_answer');    // jako iż funkcja ta zachodzi po kliknięciu i sprawdzeniu czy dana odpowiedź jest poprawna, usuwany jest kolor dodany podczas zaznaczenia odpowiedzi
+  active_buttons(); //aktywowanie przycisków żeby można było je kliknąć, zapobiega to wielokrotnemu wyborowi odpowiedzi w czasie dwu sekundowej przerwy
+  if (win_score===10){  // wygrana gry
+    disable_buttons();  //wyłączanie przycisków
   getting_question.innerHTML='<span class="information">Brawo! Udało Ci się wygrać! Oto Twój milion!</span>'+'<span class="new_game" onclick=changing_questions()>CO POWIESZ NA KOLEJNY MILION? </span>';
-  making_things_clear();
+  making_things_clear();  //resetowanie gry, możliwość ponownego używania pytań, win_score ustawiony na 0
     return;
   }
-  for (i = 0; i < win_score; i++) {
+  if(window.matchMedia("(max-width:960px)").matches){
+    getting_prize.textContent='Twoja aktualna wygrana to:'+awards[win_score];
+  }
+  else{
+  for (i = 0; i < win_score; i++) { //zwiększanie nagrody
     getting_awards[i].classList.add('active');
   };
-  changing_questions();
+};
+  changing_questions(); //zmiana pytania
 };
 
-function get_number() {
+function get_number() { //funkcja losująca liczbę odpowiadającą za zestaw pytań
   number = Math.floor((Math.random() * questions.length - 1) + 1);
 
 };
 
-function lose() {
-  this_is.classList.remove('bad_answer');
-  disable_buttons();
-  making_things_clear();
+function lose() { //przegranie gry
+  this_is.classList.remove('bad_answer'); // usuwanie 'chwilowego' kolorka podczas kliknięcia złego przycisku
+  disable_buttons();  //zapobiegnięcie kilkukrotnemu kliknięciu przycisku
+  making_things_clear();  //reset gry
 getting_question.innerHTML='<span class="information">Niestety przegrałeś!</span>'+'<span class="new_game" onclick=changing_questions(),active_buttons()>ZACZNIJ NOWĄ GRĘ! </span>';
 
 };
 
-function buttons() {
+function buttons() {  //dodawanie on clicków do przycisków
   for (i = 0; i < getting_answers.length; i++) {
-    getting_answers[i].addEventListener('click', function answering() {
-        if (this.value === questions[number].correct_answer) {
-          this.classList.add('active_answer');
-          this_is=this;
-          disable_buttons();
-          setTimeout('win()',2000);
-        } else {
-          this_is=this;
-          this.classList.add('bad_answer');
-          disable_buttons();
-          setTimeout('lose()',2000);
+    getting_answers[i].addEventListener('click', function answering() {   //onclick
+        if (this.value === questions[number].correct_answer) {    //sprawdzenie czy odpowiedź jest poprawna
+          this.classList.add('active_answer');    //chwilowy kolorek po kliknięciu
+          this_is=this;   //przekazanie aktualnego przycisku do global scope'a
+          disable_buttons();    //zapobiegnięcie kilkukrotnemu kliknięciu
+          setTimeout('win()',1000);   //małe opóźnienie gry
+        } else {  //zła odpowiedź
+          this_is=this; //przekazanie do global scope
+          this.classList.add('bad_answer'); //chwilowy kolorek
+          disable_buttons();    //zapobiegnięcie kilkukrotnemu kliknieciu
+          setTimeout('lose()',1000);  //małe opóźnienie
         };
       });
     };
 };
-function making_things_clear(){
+function making_things_clear(){ //resetowanie gry
 
-  win_score=0;
-  for(i=0;i<questions.length;i++){
+  win_score=0;    //wynik gry na 0
+  for(i=0;i<questions.length;i++){    // przywracanie wszystkich pytań do gry
     questions[i].been_or_not=false;
   }
-  for (i = 1; i < getting_awards.length; i++) {
+  for (i = 1; i < getting_awards.length; i++) {   //usuwanie nagród
     getting_awards[i].classList.remove('active');
   };
 
-  for (i = 0; i < getting_answers.length; i++){
+  for (i = 0; i < getting_answers.length; i++){ //usuwanie odpowiedzi z przycisków
   getting_answers[i].textContent ="";
     }
 }
-function check_addcolor(){
 
-
-}
-function disable_buttons(){
+function disable_buttons(){ //funkcja zapobiegająca kilkukrotnemu kliknięciu
   for(i=0;i<getting_answers.length;i++){
     getting_answers[i].classList.add('not_active');
   }
 }
-function active_buttons(){
+function active_buttons(){  //przywracanie przycisków do użytku
   for(i=0;i<getting_answers.length;i++){
     getting_answers[i].classList.remove('not_active');
   }
